9eedc351355edd49e4c00dfa3099f39407ebc218
a.alexandrakis <a.alexandrakis@datamation.gr>
2018-05-15 23:15:30 +0300

    setup flyway to create the database instead of jpa

--- src/main/java/com/codehub/spring/eshop/EShopApplication.java
+++ src/main/java/com/codehub/spring/eshop/EShopApplication.java
@@ -1,7 +1,12 @@
 package com.codehub.spring.eshop;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class EShopApplication {
@@ -9,4 +14,16 @@ public class EShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EShopApplication.class, args);
 	}
+
+
+    @Autowired
+    DataSource dataSource;
+
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        final Flyway flyway = new Flyway();
+        flyway.setDataSource(this.dataSource);
+        return flyway;
+    }
 }
