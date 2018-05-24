ac355e727db283f5c06dd6c44420d86f35f4c1f7
jpapadim <john papadimitriou @ gmail .com>
2018-05-24 22:23:12 +0300

    Swagger some configuration

--- src/main/java/com/codehub/spring/eshop/Configuration/SwaggerConfiguration.java
+++ src/main/java/com/codehub/spring/eshop/Configuration/SwaggerConfiguration.java
@@ -1,11 +1,16 @@
 package com.codehub.spring.eshop.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * Created by Dimitris on 22/5/2018.
@@ -15,11 +20,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 public class SwaggerConfiguration {
 
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.codehub.spring.eshop"))
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.codehub.spring.eshop.controller"))
+                //.paths(Predicates.not(PathSelectors.regex("/error")))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(info());
+    }
+
+    private ApiInfo info() {
+        Contact contact = new Contact("RESTFull Eshop", "localhost/e-shop", "rest@eshop.gurus");
+        return new ApiInfo("EShop Api Documentation",
+                "OpenAPI documentation",
+                "1.0",
+                "urn:tos",
+                contact,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
     }
 }
