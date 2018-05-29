323e463a1a6b5fd94a18ef98afe0b261a26c37bb
jpapadim <john papadimitriou @ gmail .com>
2018-05-29 23:53:17 +0300

    Hello Controller modification git-ignore modification

--- src/main/java/com/codehub/spring/eshop/controller/HelloController.java
+++ src/main/java/com/codehub/spring/eshop/controller/HelloController.java
@@ -3,12 +3,14 @@ package com.codehub.spring.eshop.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Created by Dimitris on 15/5/2018.
  */
 
 @RestController
+@ApiIgnore
 public class HelloController {
 
     @RequestMapping(value = "/e-shop")
@@ -17,12 +19,11 @@ public class HelloController {
 
         return "<!DOCTYPE html>\n" +
                 "<html>\n" +
-                "<body style='text-align:center;'>" +
-                "\n" +
+                "<body style='text-align:center;'>\n" +
                 "<h1>Hello World!</h1>\n" +
-                "\n" +
-                "<p>Welcome to our full Rest E-shop!</p>\n" +
-                "\n" +
+                "<p>Welcome to our full Rest E-shop! </br>\n" +
+                "<a href=\"/swagger-ui.html\">Read our OpenAPI 3.0 Documentation </a></p>\n" +
+                "</br>\n" +
                 "</body>\n" +
                 "</html>";
     }
